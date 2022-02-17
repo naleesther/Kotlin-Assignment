52b2731dade539e4c808c5187d9ba68d5f9bc12a
aluochvivian <aluochviviann@gmail.com>
2022-02-17 11:02:28 +0300

    first commit

---
+++ src/data.kts
@@ -0,0 +1,12 @@
+fun main(){
+   var name: String=("sharon wangare")
+    println(name)
+    var age="19"
+    println(age)
+    var phonenumber="0768546578"
+    println(phonenumber)
+    var weight_in_kg="50"
+    println(weight_in_kg)
+    var kenya: Boolean=true
+    println(kenya)
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+fun main(){
+        var name: String=("sharon wangare")
+        println(name)
+        var age="19"
+        println(age)
+        var phonenumber="0768546578"
+        println(phonenumber)
+        var weight_in_kg="50"
+        println(weight_in_kg)
+        val kenya: Boolean=true
+        println(kenya)
+        val notkenyan: Boolean=false
+        println(notkenyan)
+
+}
+
